4ed146fa3aeda8830c52094cfe6a5a732147b3ca
Nguyen Hong Quan <quannhpd09461@fpt.edu.vn>
2023-02-13 20:06:03 +0700

    Quan

--- Lab5 and 6/bai1.c
+++ Lab5 and 6/bai1.c
@@ -11,20 +11,35 @@ int main()
     printf("Nhap ten hoc sinh: ");
     scanf("%s", &name);
 
-    // Yêu cầu người dùng nhập điểm toán
+    // Yêu cầu người dùng nhập điểm toán với điều kiện kiểm tra
     printf("Nhap diem toan: ");
     scanf("%f", &toan);
+    while (toan < 0 || toan > 10)
+    {
+        printf("Diem toan khong hop le! Nhap lai: ");
+        scanf("%f", &toan);
+    }
 
-    // Yêu cầu người dùng nhập điểm lý
+    // Yêu cầu người dùng nhập điểm lý với điều kiện kiểm tra
     printf("Nhap diem ly: ");
     scanf("%f", &ly);
+    while (ly < 0 || ly > 10)
+    {
+        printf("Diem ly khong hop le! Nhap lai: ");
+        scanf("%f", &ly);
+    }
 
-    // Yêu cầu người dùng nhập điểm hóa
+    // Yêu cầu người dùng nhập điểm hóa với điều kiện kiểm tra
     printf("Nhap diem hoa: ");
     scanf("%f", &hoa);
+    while (hoa < 0 || hoa > 10)
+    {
+        printf("Diem hoa khong hop le! Nhap lai: ");
+        scanf("%f", &hoa);
+    }
 
     // Tính điểm trung bình
-    float TB =(toan + ly + hoa) / 3;
+    float TB = (toan + ly + hoa) / 3;
     switch ((int)TB)
     {
     case 10:
@@ -39,16 +54,8 @@ int main()
     case 5:
         printf("%s dat loai Loai C", name);
         break;
-    case 4:
-    case 3:
-    case 2:
-    case 1:
-    case 0:
-        printf("%s dat loai Loai D", name);
-        break;
-
     default:
-        printf("Diem khong hop le!");
+        printf("%s dat loai Loai D", name);
         break;
     }
     return 0;
